44bd3443a102f1a5b27d6493ba91246c79359253
Alan <alanaw1@berkeley.edu>
2021-08-09 22:28:18 -0700

    Make sure Python script is consistent with PEP8 style

--- data-analysis/1KG/test_exchangeability.py
+++ data-analysis/1KG/test_exchangeability.py
@@ -7,16 +7,23 @@ Created on Fri Aug  6 21:30:17 2021
 
 @author: Alan Aw
 """
+# Load libraries
+
+import argparse
+import os
+from datetime import datetime
+import gc
+import flintypy
+from scipy.spatial.distance import pdist
+from bed_reader import open_bed
 
 # Arguments pick up the variables passed from command line
 # ARG1 = directory to plink input files
 # ARG2 = directory to plink
 # ARG3 = number of permutations
-# Ex: 
+# Ex:
 
 # Create parser to parse user-provided arguments
-import argparse
-
 parser = argparse.ArgumentParser()
 req_grp = parser.add_argument_group(title = "Required arguments")
 req_grp.add_argument("--input", "-i", dest = "input", help = "PLINK input files",
@@ -32,14 +39,6 @@ INPUT_FILE_DIR = args.input
 PLINK_DIR = args.plink
 NUM_PERMS = args.num_perms
 
-# Load libraries
-import os
-from datetime import datetime
-import flintypy
-from scipy.spatial.distance import pdist
-from bed_reader import open_bed
-import gc
-
 # For each chromosome
 print(f"{datetime.now()}: Generating distance matrices for each chromosome...")
 dist_list = []
@@ -47,22 +46,22 @@ for b in range(1,23):
     print(f"Computing pairwise distances for Chr{b}")
     start = datetime.now()
     # Use PLINK to filter genos to that chrom only
-    os.system(PLINK_DIR + " --bfile " + INPUT_FILE_DIR + 
-              " --chr " + str(b) + " --make-bed --out " + 
+    os.system(PLINK_DIR + " --bfile " + INPUT_FILE_DIR +
+              " --chr " + str(b) + " --make-bed --out " +
               INPUT_FILE_DIR + "_" + str(b))
-    
+
     # Load PLINK file using pybedtools
     bed_file = open_bed(INPUT_FILE_DIR + "_" + str(b) + ".bed")
-    
+
     # Compute pairwise distances
-    pairwise_dists = pdist(bed_file.read(), 'cityblock') 
-    
+    pairwise_dists = pdist(bed_file.read(), 'cityblock')
+
     # Append to dist_list
     dist_list.append(pairwise_dists)
-    
+
     # Force clear garbage
     gc.collect()
-    
+
     # Print time elapsed
     print(f"Time elapsed: {(datetime.now() - start).seconds} sec")
 
@@ -75,4 +74,5 @@ print(f"{datetime.now()}: Computing exchangeability test p-value...")
 start = datetime.now()
 p_value = flintypy.v_stat.dist_data_p_value(dist_list, num_perms = NUM_PERMS)
 print(f"Exchangeability p-value: {p_value}")
-print(f"Time elapsed: {(datetime.now() - start).microseconds} microsec, or {(datetime.now() - start).seconds} sec")
+print(f"Time elapsed: {(datetime.now() - start).microseconds} microsec, \
+      or {(datetime.now() - start).seconds} sec")
